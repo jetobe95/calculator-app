ad591c63634a70c18ab3644020e5a9cb4010fdf7
jeffrey.torres <jeffrey.torres@pragma.com.co>
2021-07-21 15:19:58 -0500

    uso de (prefers-color-scheme: light)

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,28 +1,34 @@
-import { useEffect, useState } from 'react';
-import { setToLS, getFromLS } from '../utils/storage';
-import _ from 'lodash';
-import { Theme } from '../theme/types';
+import { useEffect, useState } from "react";
+import { setToLS, getFromLS } from "../utils/storage";
+import _ from "lodash";
+import { Theme } from "../theme/types";
 
 export const useTheme = () => {
-  const themes = getFromLS('all-themes');
+  const themes = getFromLS("all-themes");
   const [theme, setTheme] = useState<Theme>(themes?.data.light);
   const [themeLoaded, setThemeLoaded] = useState(false);
 
-  const setMode = (mode:Theme) => {
-    setToLS('theme', mode)
+  const setMode = (mode: Theme) => {
+    setToLS("theme", mode);
     setTheme(mode);
   };
 
   const getFonts = () => {
-    const allFonts = _.values(_.mapValues(themes.data, 'font'));
+    const allFonts = _.values(_.mapValues(themes.data, "font"));
     return allFonts;
-  }
+  };
 
-  useEffect(() =>{
-    const localTheme = getFromLS('theme');
-    localTheme ? setTheme(localTheme) : setTheme(themes.data.orange);
+  useEffect(() => {
+    const localTheme = getFromLS("theme");
+    let isLightTheme = false;
+    if (typeof window != "undefined") {
+      isLightTheme = window.matchMedia("(prefers-color-scheme: light)").matches;
+    }
+    localTheme
+      ? setTheme(localTheme)
+      : setTheme(isLightTheme ? themes.data.light : themes.data.dark);
     setThemeLoaded(true);
   }, []);
 
-  return { theme,themes, themeLoaded, setMode, getFonts };
+  return { theme, themes, themeLoaded, setMode, getFonts };
 };
